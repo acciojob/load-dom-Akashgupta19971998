df94c69f34d79c04e8d6ce41bd1a07c44cbf72e5
Akashgupta19971998 <akashguptaakash2@gmail.com>
2023-04-13 12:04:01 +0530

    commit

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+//console.log("hi")
+let op=document.getElementById("dom");
+function output(){
+    //console.log("hi")
+    op.innerHTML="DOM load success"
+}
+output();
